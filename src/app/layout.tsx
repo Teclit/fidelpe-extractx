af3621bb942c1fbd7e1860f3b0d80410bdc66c73
Teklit <t.tewolde@tx-studio.com>
2025-10-25 13:52:22 +0200

    Update layout and metadata for improved accessibility and font integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,26 @@
 // src/app/layout.tsx
 import "./globals.css";
-import { ReactNode } from "react";
-import type { Metadata } from "next";
+import {ReactNode} from "react";
+import type {Metadata} from "next";
+import localFont from 'next/font/local';
+
+
+const waldba = localFont({
+    src: './fonts/RaeyType/WaldbaYigezuBisratGothic-Regular.ttf',
+    display: 'swap',
+});
+
 
 export const metadata: Metadata = {
-    title: "FidelpeExtractX - Simplifier les textes",
-    description: "Outil simple pour extraire et rendre les textes plus faciles à comprendre, conçu pour les personnes peu à l'aise avec le numérique.",
+    title: "FidelpeExtractX - Simplify texts",
+    description: "Simple tool to extract and simplify texts, designed for people who are not comfortable with digital technology."
 };
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayout({children}: { children: ReactNode }) {
     return (
-        <html lang="fr">
+        <html lang="en" className={waldba.className}>
         <body className="app-container">
-            <main className="card">{children}</main>
+        <main className="card">{children}</main>
         </body>
         </html>
     );
